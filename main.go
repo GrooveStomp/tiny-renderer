c216db0ec87914f40a7d3bffce9df79588b1fe8a
GrooveStomp <aaron.oman@gmail.com>
2017-02-16 11:38:31 -0800

    Lesson 2: Use the faster triangle sweep algorithm

--- main.go
+++ main.go
@@ -38,11 +38,11 @@ func (c *color) Set(r, g, b, a uint32) {
 
 type image struct {
 	Pixels []color
-	Width  uint
-	Height uint
+	Width  int
+	Height int
 }
 
-func MakeImage(width, height uint) *image {
+func MakeImage(width, height int) *image {
 	var img image
 	img.Pixels = make([]color, width*height)
 	img.Width = width
@@ -50,11 +50,11 @@ func MakeImage(width, height uint) *image {
 	return &img
 }
 
-func (img image) Get(x, y uint) color {
+func (img image) Get(x, y int) color {
 	return img.Pixels[y*img.Width+x]
 }
 
-func (img *image) Set(x, y uint, c color) {
+func (img *image) Set(x, y int, c color) {
 	if x >= img.Width {
 		panic(fmt.Sprintf("x(%v) is out of range!", x))
 	} else if y >= img.Height {
@@ -64,9 +64,9 @@ func (img *image) Set(x, y uint, c color) {
 }
 
 func (img *image) FlipVertical() {
-	for y := uint(0); y < (img.Height/2)+1; y++ {
+	for y := 0; y < (img.Height/2)+1; y++ {
 		y2 := (img.Height - 1) - y
-		for x := uint(0); x < img.Width; x++ {
+		for x := 0; x < img.Width; x++ {
 			c1 := img.Get(x, y)
 			c2 := img.Get(x, y2)
 			img.Set(x, y, c2)
@@ -76,8 +76,8 @@ func (img *image) FlipVertical() {
 }
 
 func (img *image) Fill(c color) {
-	for y := uint(0); y < img.Height; y++ {
-		for x := uint(0); x < img.Width; x++ {
+	for y := 0; y < img.Height; y++ {
+		for x := 0; x < img.Width; x++ {
 			img.Set(x, y, c)
 		}
 	}
@@ -86,11 +86,11 @@ func (img *image) Fill(c color) {
 func (src *image) WritePng(filename string) error {
 	img := goimg.NewRGBA(goimg.Rectangle{goimg.Point{0, 0}, goimg.Point{int(src.Width), int(src.Height)}})
 
-	for y := uint(0); y < src.Height; y++ {
+	for y := 0; y < src.Height; y++ {
 		y2 := (src.Height - 1) - y
-		for x := uint(0); x < src.Width; x++ {
+		for x := 0; x < src.Width; x++ {
 			r, g, b, a := src.Get(x, y).Rgba()
-			img.Set(int(x), int(y2), gocolor.RGBA{uint8(r), uint8(g), uint8(b), uint8(a)})
+			img.Set(x, y2, gocolor.RGBA{uint8(r), uint8(g), uint8(b), uint8(a)})
 		}
 	}
 
@@ -102,6 +102,18 @@ func (src *image) WritePng(filename string) error {
 	file.Close()
 
 	return nil
+}
+
+func (img *image) HorizontalLine(x0, x1 int, y int, c color) {
+	if x0 > x1 {
+		t := x0
+		x0 = x1
+		x1 = t
+	}
+
+	for x := x0; x <= x1; x++ {
+		img.Set(x, y, c)
+	}
 }
 
 func (img *image) Line(x0, y0, x1, y1 int, c color) {
@@ -146,9 +158,9 @@ func (img *image) Line(x0, y0, x1, y1 int, c color) {
 
 	for x := x0; x <= x1; x++ {
 		if steep {
-			img.Set(uint(y), uint(x), c)
+			img.Set(y, x, c)
 		} else {
-			img.Set(uint(x), uint(y), c)
+			img.Set(x, y, c)
 		}
 
 		err2 += derr2
@@ -163,8 +175,65 @@ func (img *image) Line(x0, y0, x1, y1 int, c color) {
 	}
 }
 
-func (img *image) Triangle(t Triangle, c color) {
-	clamp   := obj.Vertex{float64(img.Width - 1), float64(img.Height - 1), float64(0)}
+func (img *image) TriangleSweep(t Triangle, c color) {
+	v0, v1, v2 := t.v0, t.v1, t.v2
+
+	swap := func(v0, v1 *obj.Vertex) {
+		t := obj.Vertex{v0.X, v0.Y, v0.Z}
+		v0.X = v1.X
+		v0.Y = v1.Y
+		v0.Z = v1.Z
+		v1.X = t.X
+		v1.Y = t.Y
+		v1.Z = t.Z
+	}
+
+	// Bubblesort. Sort vertices by Y-component, ascending order.
+	if v0.Y > v1.Y {
+		swap(&v0, &v1)
+	}
+	if v0.Y > v2.Y {
+		swap(&v0, &v2)
+	}
+	if v1.Y > v2.Y {
+		swap(&v1, &v2)
+	}
+
+	totalHeight := v2.Y - v0.Y
+
+	for y := v0.Y; y <= v1.Y; y++ {
+		segmentHeight := v1.Y - v0.Y + 1
+		alpha := (y - v0.Y) / totalHeight
+		beta := (y - v0.Y) / segmentHeight
+
+		a := obj.Add(v0, obj.Multiply(obj.Subtract(v2, v0), alpha))
+		b := obj.Add(v0, obj.Multiply(obj.Subtract(v1, v0), beta))
+
+		if a.X > b.X {
+			swap(&a, &b)
+		}
+
+		img.HorizontalLine(int(a.X), int(b.X), int(y), c)
+	}
+
+	for y := v1.Y; y <= v2.Y; y++ {
+		segmentHeight := v2.Y - v1.Y + 1
+		alpha := (y - v0.Y) / totalHeight
+		beta := (y - v1.Y) / segmentHeight
+
+		a := obj.Add(v0, obj.Multiply(obj.Subtract(v2, v0), alpha))
+		b := obj.Add(v1, obj.Multiply(obj.Subtract(v2, v1), beta))
+
+		if a.X > b.X {
+			swap(&a, &b)
+		}
+
+		img.HorizontalLine(int(a.X), int(b.X), int(y), c)
+	}
+}
+
+func (img *image) TriangleBarycentric(t Triangle, c color) {
+	clamp := obj.Vertex{float64(img.Width - 1), float64(img.Height - 1), float64(0)}
 
 	xs := []float64{t.v0.X, t.v1.X, t.v2.X, 0, float64(img.Width - 1)}
 	ys := []float64{t.v0.Y, t.v1.Y, t.v2.Y, 0, float64(img.Height - 1)}
@@ -173,16 +242,15 @@ func (img *image) Triangle(t Triangle, c color) {
 	sort.Float64s(ys)
 
 	bboxMin := obj.Vertex{math.Max(0, xs[0]), math.Max(0, ys[0]), float64(0)}
-	bboxMax := obj.Vertex{math.Min(clamp.X, xs[len(xs) - 1]), math.Min(clamp.Y, ys[len(ys) - 1]), float64(0)}
+	bboxMax := obj.Vertex{math.Min(clamp.X, xs[len(xs)-1]), math.Min(clamp.Y, ys[len(ys)-1]), float64(0)}
 
-	var p obj.Vertex
-	for p.X = bboxMin.X; p.X <= bboxMax.X; p.X++ {
-		for p.Y = bboxMin.Y; p.Y <= bboxMax.Y; p.Y++ {
-			bc := t.Barycentric(p)
+	for x := int(math.Floor(bboxMin.X)); x <= int(math.Ceil(bboxMax.X)); x++ {
+		for y := int(math.Floor(bboxMin.Y)); y <= int(math.Ceil(bboxMax.Y)); y++ {
+			bc := t.Barycentric(obj.Vertex{float64(x), float64(y), float64(0)})
 			if bc.X < 0 || bc.Y < 0 || bc.Z < 0 {
 				continue
 			}
-			img.Set(uint(p.X), uint(p.Y), c)
+			img.Set(int(x), int(y), c)
 		}
 	}
 }
@@ -200,9 +268,11 @@ func (t *Triangle) Barycentric(v obj.Vertex) obj.Vertex {
 	v1 := obj.Vertex{t.v2.Y - t.v0.Y, t.v1.Y - t.v0.Y, t.v0.Y - v.Y}
 	vu := obj.CrossProduct(v0, v1)
 
-	if math.Abs(vu.Y) < 1 { return obj.Vertex{-1,1,1} }
+	if math.Abs(vu.Y) < 1 {
+		return obj.Vertex{-1, 1, 1}
+	}
 
-	return obj.Vertex{1.0 - (vu.X + vu.Y) / vu.Z, vu.Y / vu.Z, vu.X / vu.Z}
+	return obj.Vertex{1.0 - (vu.X+vu.Y)/vu.Z, vu.Y / vu.Z, vu.X / vu.Z}
 }
 
 func main() {
@@ -210,8 +280,8 @@ func main() {
 
 	lightDir := obj.Vertex{0, 0, -1}
 
-	width := uint(1000)
-	height := uint(1000)
+	width := 1000
+	height := 1000
 
 	img := MakeImage(width, height)
 	img.Fill(0x000000FF)
@@ -226,7 +296,7 @@ func main() {
 		var worldCoords [3]obj.Vertex
 
 		for j := 0; j < 3; j++ {
-			v := model.Vertices[face[j] - 1]
+			v := model.Vertices[face[j]-1]
 			x := (v.X + 1) * (float64(width-1) / 2)
 			y := (v.Y + 1) * (float64(height-1) / 2)
 			screenCoords[j] = obj.Vertex{x, y, float64(0)}
@@ -238,8 +308,8 @@ func main() {
 		intensity := obj.DotProduct(n, lightDir)
 		if intensity > 0 {
 			var c color
-			c.Set(uint32(intensity * 255), uint32(intensity * 255), uint32(intensity * 255), 255)
-			img.Triangle(Triangle{screenCoords[0], screenCoords[1], screenCoords[2]}, c)
+			c.Set(uint32(intensity*255), uint32(intensity*255), uint32(intensity*255), 255)
+			img.TriangleSweep(Triangle{screenCoords[0], screenCoords[1], screenCoords[2]}, c)
 		}
 	}
 
